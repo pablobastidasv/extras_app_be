304a0c85c9a4a61cfc6039372ee007d492e727d9
Daniel Posada <daniel.posada@contractors.birchbox.com>
2017-09-04 16:07:42 -0500

    Match Method

--- src/main/kotlin/co/vpthon/extras/Control.kt
+++ src/main/kotlin/co/vpthon/extras/Control.kt
@@ -9,17 +9,25 @@ class Controller {
         val params : HashMap<String, Object> = HashMap<String, Object>();
         val cypher = StringJoiner(",", "MATCH ", " RETURN person")
 
-
-        //Gender, City, Eyes, Ethnicity, Height, BodyType,
         if (!query.data.gender.isNullOrEmpty()) {
-            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.Gender.name}', value: '${query.data.gender}' }),");
+            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.Gender.name}', value: '${query.data.gender}' })");
         }
-
-
+        if (!query.data.city.isNullOrEmpty()) {
+            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.City.name}', value: '${query.data.city}' })");
+        }
+        if (!query.data.eyes.isNullOrEmpty()) {
+            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.Eyes.name}', value: '${query.data.eyes}' })");
+        }
+        if (!query.data.ethnicity.isNullOrEmpty()) {
+            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.Ethnicity.name}', value: '${query.data.ethnicity}' })");
+        }
+        if (!query.data.bodyType.isNullOrEmpty()) {
+            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.BodyType.name}', value: '${query.data.bodyType}' })");
+        }
+        
         val people = Neo4jSessionFactory.instance.openSession().query(cypher.toString(), params);
 
         return ArrayList<Person>()
     }
-
 
 }
