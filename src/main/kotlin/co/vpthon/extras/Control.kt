c60cdcc647465e14caa2e9118aeebecc10b2880e
Daniel Posada <daniel.posada@contractors.birchbox.com>
2017-09-04 15:43:40 -0500

    Match Method

--- src/main/kotlin/co/vpthon/extras/Control.kt
+++ src/main/kotlin/co/vpthon/extras/Control.kt
@@ -3,6 +3,17 @@ package co.vpthon.extras
 class Controller {
 
     fun matchPeople(query: Query) : List<Person>{
+
+        val params : HashMap<String, Object> = HashMap<String, Object>();
+        var cypher = "";
+
+        val people = Neo4jSessionFactory.instance.openSession().query(cypher, params);
+
+        if (!query.data.gender.isNullOrEmpty()) {
+            cypher+="";
+        }
+
+
         return ArrayList<Person>()
     }
 
