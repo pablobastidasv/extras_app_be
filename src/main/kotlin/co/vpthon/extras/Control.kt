83838b52f5f09ecce2474f6a3658148e71228569
Pablo Bastidas <pablobastidasv@gmail.com>
2017-09-04 15:18:44 -0500

    Object mapping neo4j

--- src/main/kotlin/co/vpthon/extras/Control.kt
+++ src/main/kotlin/co/vpthon/extras/Control.kt
@@ -1,5 +1,7 @@
 package co.vpthon.extras
 
+import org.neo4j.ogm.session.Session
+
 class Controller {
 
     fun matchPeople(query: Query) : List<Person>{
@@ -9,3 +11,13 @@ class Controller {
 
 }
 
+
+
+fun main(args: Array<String>) {
+    val session: Session = Neo4jSessionFactory.instance.openSession()
+
+    val person = session.loadAll(Person::class.java)
+    println(person)
+    val products = session.loadAll(Attribute::class.java)
+    println(products)
+}
--- src/main/kotlin/co/vpthon/extras/Mocks.kt
+++ src/main/kotlin/co/vpthon/extras/Mocks.kt
@@ -3,28 +3,28 @@ package co.vpthon.extras
 fun createPeople() : List<Person>{
 
   val testPeople = ArrayList<Person>(6)
-  testPeople.add(Person("Marcus",  createAttributes(), 12345678L))
-  testPeople.add(Person("Kevin",   createAttributes(), 8765432L))
-  testPeople.add(Person("Julian",  createAttributes(), 22348790L))
-  testPeople.add(Person("Pablo",   createAttributes(), 34212L))
-  testPeople.add(Person("Daniel",  createAttributes(), 678902L))
-  testPeople.add(Person("Agustin", createAttributes(), 216721812L))
+  testPeople.add(Person(name="Marcus",  attributes = createAttributes(), id = 12345678L))
+  testPeople.add(Person(name="Kevin",   attributes = createAttributes(), id = 8765432L))
+  testPeople.add(Person(name="Julian",  attributes = createAttributes(), id = 22348790L))
+  testPeople.add(Person(name="Pablo",   attributes = createAttributes(), id = 34212L))
+  testPeople.add(Person(name="Daniel",  attributes = createAttributes(), id = 678902L))
+  testPeople.add(Person(name="Agustin", attributes = createAttributes(), id = 216721812L))
   return testPeople
 }
 
 fun createAttributes() : HashSet<Attribute> {
   val attributes = HashSet<Attribute>(12)
-  attributes.add(Attribute(AttributeType.BodyType,  "gordo"))
-  attributes.add(Attribute(AttributeType.Age,       "20"))
-  attributes.add(Attribute(AttributeType.City,      "Medallo!"))
-  attributes.add(Attribute(AttributeType.Ethnicity, "Rolo"))
-  attributes.add(Attribute(AttributeType.Eyes,      "Azules"))
-  attributes.add(Attribute(AttributeType.Gender,    "Masculino"))
-  attributes.add(Attribute(AttributeType.HairColor, "purple"))
-  attributes.add(Attribute(AttributeType.HairLength,"largo"))
-  attributes.add(Attribute(AttributeType.Height,    "chaparro"))
-  attributes.add(Attribute(AttributeType.Nose,      "En Garfio"))
-  attributes.add(Attribute(AttributeType.Skin,      "Blanco"))
-  attributes.add(Attribute(AttributeType.Tatoo,     "All over"))
+  attributes.add(Attribute(type = AttributeType.BodyType,  value = "gordo"))
+  attributes.add(Attribute(type = AttributeType.Age,       value = "20"))
+  attributes.add(Attribute(type = AttributeType.City,      value = "Medallo!"))
+  attributes.add(Attribute(type = AttributeType.Ethnicity, value = "Rolo"))
+  attributes.add(Attribute(type = AttributeType.Eyes,      value = "Azules"))
+  attributes.add(Attribute(type = AttributeType.Gender,    value = "Masculino"))
+  attributes.add(Attribute(type = AttributeType.HairColor, value = "purple"))
+  attributes.add(Attribute(type = AttributeType.HairLength,value = "largo"))
+  attributes.add(Attribute(type = AttributeType.Height,    value = "chaparro"))
+  attributes.add(Attribute(type = AttributeType.Nose,      value = "En Garfio"))
+  attributes.add(Attribute(type = AttributeType.Skin,      value = "Blanco"))
+  attributes.add(Attribute(type = AttributeType.Tatoo,     value = "All over"))
   return attributes
 }
