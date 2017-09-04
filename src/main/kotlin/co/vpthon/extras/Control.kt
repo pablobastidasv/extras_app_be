13136a13acbab4eab9196366bb9db2cc2f5762ae
marcuss <marcus.sanchez@contractors.birchbox.com>
2017-09-04 16:09:55 -0500

    Missing filter properties.

--- src/main/kotlin/co/vpthon/extras/Control.kt
+++ src/main/kotlin/co/vpthon/extras/Control.kt
@@ -7,14 +7,41 @@ class Controller {
     fun matchPeople(query: Query) : List<Person>{
 
         val params : HashMap<String, Object> = HashMap<String, Object>();
-        val cypher = StringJoiner(",", "MATCH ", " RETURN person")
+        val cypher = StringJoiner(", ", "MATCH ", " RETURN person")
 
-
-        //Gender, City, Eyes, Ethnicity, Height, BodyType,
         if (!query.data.gender.isNullOrEmpty()) {
-            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.Gender.name}', value: '${query.data.gender}' }),");
+            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.Gender.name}', value: '${query.data.gender}' })");
         }
 
+        if (!query.data.tattoo.isNullOrEmpty()) {
+            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.Tattoo.name}', value: '${query.data.tattoo}' })");
+        }
+
+        if (!query.data.gender.isNullOrEmpty()) {
+            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.Age.name}', value: '${query.data.age}' })");
+        }
+
+
+        if (!query.data.skin.isNullOrEmpty()) {
+            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.Skin.name}', value: '${query.data.skin}' })");
+        }
+
+
+        if (!query.data.ethnicity.isNullOrEmpty()) {
+            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.Ethnicity.name}', value: '${query.data.ethnicity}' })");
+        }
+
+        if (!query.data.nose.isNullOrEmpty()) {
+            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.Nose.name}', value: '${query.data.nose}' })");
+        }
+
+        if (!query.data.hairColor.isNullOrEmpty()) {
+            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.HairColor.name}', value: '${query.data.hairColor}' })");
+        }
+
+        if (!query.data.hairLength.isNullOrEmpty()) {
+            cypher.add("(person:Person)-[:HAS]-(:Attribute{type: '${AttributeType.HairLength.name}', value: '${query.data.hairLength}' })");
+        }
 
         val people = Neo4jSessionFactory.instance.openSession().query(cypher.toString(), params);
 
