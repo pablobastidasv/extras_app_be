1239d3ac8dfc9c47b39ac31ab9734eef01c9b6d8
Pablo Bastidas <pablo.bastidas@contractors.birchbox.com>
2017-09-04 11:50:54 -0500

    Neo4JConfig

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # extras_app_be
+
+![alt text](readme/intellijIdea_RunnerConfig.png "Intellij idea configuration")
--- src/main/kotlin/co/vpthon/extras/Neo4JConfig.kt
+++ src/main/kotlin/co/vpthon/extras/Neo4JConfig.kt
@@ -1,10 +1,26 @@
 package co.vpthon.extras
 
+import org.neo4j.ogm.config.Configuration
 import org.neo4j.ogm.session.SessionFactory
+
+class Neo4JConfig{
+    val config: Configuration = Configuration()
+
+    init{
+        val password: String = System.getenv("NEO4J_PASS")
+        val server: String = System.getenv("NEO4J_SERVER")
+
+        config.driverConfiguration()
+        config.driverConfiguration().driverClassName = "org.neo4j.ogm.drivers.bolt.driver.BoltDriver"
+        config.driverConfiguration().uri = "bolt://$server"
+        config.driverConfiguration().connectionPoolSize = 150
+        config.driverConfiguration().setCredentials("neo4j", password)
+    }
+}
 
 class Neo4jSessionFactory private constructor(){
     private object Holder {
-        val INSTANCE = SessionFactory("co.vpnthon.extras")
+        val INSTANCE = SessionFactory(Neo4JConfig().config, "co.vpnthon.extras")
     }
 
     companion object {
