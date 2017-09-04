e11dba6e5d22c816480a86d04a75e52340160237
Daniel Posada <daniel.posada@contractors.birchbox.com>
2017-09-04 10:22:53 -0500

    Model

--- src/main/kotlin/co/vpthon/extras/Model.kt
+++ src/main/kotlin/co/vpthon/extras/Model.kt
@@ -5,5 +5,5 @@ data class Person(val id: Long, val name: String)
 data class Attribute(val id: Long, val type: String, val value: String)
 
 enum class AttributeType {
-    Gender,	City, Eyes, Ethnicity, Height, Build, HairColor, HairLength, Nose, Skin, Age, Tatoo
+    Gender,	City, Eyes, Ethnicity, Height, BodyType, HairColor, HairLength, Nose, Skin, Age, Tatoo
 }
