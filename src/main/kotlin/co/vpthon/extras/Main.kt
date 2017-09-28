b15c8a10f6f30188a828922713ffd69ef1958f6f
Pablo Bastidas <pablobastidasv@gmail.com>
2017-09-27 20:30:56 -0500

    Heroku integration 2

--- src/main/kotlin/co/vpthon/extras/Main.kt
+++ src/main/kotlin/co/vpthon/extras/Main.kt
@@ -2,10 +2,13 @@ package co.vpthon.extras
 
 import com.google.gson.Gson
 import spark.Spark.before
+import spark.Spark.port
 
 val gson: Gson = Gson()
 
 fun main(args: Array<String>) {
+
+  port(obtainPort())
 
   before("*", { _, res -> res.type("application/json") })
 
@@ -13,3 +16,5 @@ fun main(args: Array<String>) {
   PeopleResources().load()
   AttributesResources().load()
 }
+
+fun obtainPort(): Int = ProcessBuilder().environment()["PORT"]?.toInt() ?: 4567
